ccf1e25d421bc436c6689d4bcafa9b2bd0f81008
TheGui <g0u0i@hotmail.com>
2017-03-02 15:26:54 -0300

    Add project files.

---
+++ Snake/Jogo.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Snake {
+    public class Jogo
+    {
+
+        public enum Direcao
+        {
+            Left, Up, Right, Down, Nenhuma
+        }
+        public int tamanho;
+        public List<Rectangle> rects = new List<Rectangle>();
+        public Direcao direction;
+        public Direcao subDir1 = Direcao.Nenhuma;
+        public Direcao subDir2 = Direcao.Nenhuma;
+        const int dimension = 10;
+        Point posição;
+        public Rectangle food;
+        Canvas canvas;
+        CheckBox parede;
+        Timer t;
+
+        public SolidColorBrush foodColor = (SolidColorBrush)(new BrushConverter().ConvertFrom("#00ff00"));
+        public SolidColorBrush foodStrokeColor;
+        public SolidColorBrush blackColor;
+
+        public SolidColorBrush rectColor;
+        public SolidColorBrush rectStrokeColor;
+        public SolidColorBrush hitColor = new SolidColorBrush(Colors.Red);
+
+        //Pega as variaveis da MainWindow e armazena nesta classe
+        public Jogo(Canvas canvas, Timer t, CheckBox parede)
+        {
+            this.parede = parede;
+            this.t = t;
+            this.canvas = canvas;
+
+            food = foodRect();
+
+            waveOutSetVolume(IntPtr.Zero, uint.MaxValue);
+        }
+
+        //Metodo que adiciona os retangulos da cobra e diz qual é a frente e a direção
+        public void createBaseRects()
+        {
+            rects.Add(refRect(2, 2));
+            rects.Add(refRect(3, 2));
+            rects.Add(refRect(4, 2));
+            rects.Add(refRect(5, 2));
+            rects.Add(refRect(6, 2));
+            rects.Add(refRect(7, 2));
+            rects.Add(refRect(8, 2));
+            rects.Add(refRect(9, 2));          
+
+            posição = new Point(9 * dimension, 2 * dimension);
+
+            canvas.Children.Add(food);
+            foreach (var x in rects)
+            {
+                canvas.Children.Add(x);
+            }
+
+            direction = Direcao.Right;
+        }
+
+        //Metodo que adiciona os retangulos da cobra
+        public Rectangle refRect(double x, double y)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Fill = rectColor;
+            rect.Stroke = rectStrokeColor;
+            rect.Width = dimension;
+            rect.Height = dimension;
+            Canvas.SetLeft(rect, x * dimension);
+            Canvas.SetTop(rect, y * dimension);
+            return rect;
+        }
+
+        //Metodo que adiciona o retangulo da comida
+        public Rectangle foodRect()
+        {
+            Random x = new Random();
+            Rectangle rect = new Rectangle();
+            rect.Fill = foodColor;
+            rect.Stroke = foodStrokeColor;
+            rect.StrokeThickness = 1;
+            rect.Width = dimension;
+            rect.Height = dimension;
+            Canvas.SetLeft(rect, x.Next(30) * dimension);
+            Canvas.SetTop(rect, x.Next(30) * dimension);
+            return rect;
+        }
+
+        //Tick do jogo
+        public void tick2()
+        {
+            //Stopwatch para testar performance
+            Stopwatch st1 = new Stopwatch();
+            st1.Start();
+
+            //Pega as direções armazenadas e joga pra direção atual
+            if (subDir1 != Direcao.Nenhuma)
+            {
+                direction = subDir1;
+                subDir1 = Direcao.Nenhuma;
+            }
+            if (subDir2 != Direcao.Nenhuma)
+            {
+                subDir1 = subDir2;
+                subDir2 = Direcao.Nenhuma;
+            }
+
+            //Move snake para a proxima posição e executa o metodo
+            if (direction == Direcao.Left)
+            {
+                posição.X -= dimension;
+                moveRect();
+            }
+            else
+            if (direction == Direcao.Right)
+            {
+                posição.X += dimension;
+                moveRect();
+            }
+            else
+            if (direction == Direcao.Up)
+            {
+                posição.Y -= dimension;
+                moveRect();
+            }
+            else
+            if (direction == Direcao.Down)
+            {
+                posição.Y += dimension;
+                moveRect();
+            }
+
+            //Passa por todos os retangulos para mudar sua cor caso mude de tema e mudar o tamanho da Border
+            PointCollection d = new PointCollection();
+            for (int i = 0; i < rects.Count; i++)
+            {
+                d.Add(new Point(Canvas.GetLeft(rects[i]), Canvas.GetTop(rects[i])));
+                rects[i].StrokeThickness = i / (double)rects.Count;
+                if (i == rects.Count - 1)
+                {
+                    rects[i].StrokeThickness = 1.2;
+                }
+                if (rects[i].Fill != rectColor)
+                {
+                    rects[i].Fill = rectColor;
+                }
+                if (rects[i].Stroke != rectStrokeColor)
+                {
+                    rects[i].Stroke = rectStrokeColor;
+                }
+            }
+
+            //Caso a cobra bata em si mesma
+            if (d.Where(l => l == posição).Count() > 1)
+            {
+                Canvas.SetZIndex(rects.Last(), int.MaxValue);
+                rects.Last().Fill = hitColor;
+                t.Stop();
+                MessageBox.Show("Snake is kill");
+                t.Dispose();
+                return;
+            }
+
+            //Caso saia do quadrado
+            if (posição.X > 29 * dimension || posição.Y > 29 * dimension || posição.X < 0 || posição.Y < 0)
+            {
+                rects.Last().Fill = hitColor;
+                t.Stop();
+                MessageBox.Show("Snake is kill");
+                t.Dispose();                
+            }
+
+            //Teste de performance
+            st1.Stop();
+            Debug.WriteLine(st1.Elapsed);
+        }
+
+        //Metodo que move o ultimo retangulo para a proxima casa
+        public void moveRect()
+        {
+            //Move snake para a posição oposta do canvas caso saia do proprio e a checkbox não esteja checada
+            if (!parede.IsChecked.Value)
+            {
+                if (posição.X > 29 * dimension)
+                {
+                    posição.X = 0;
+                }
+                if (posição.X < 0)
+                {
+                    posição.X = 29 * dimension;
+                }
+                if (posição.Y > 29 * dimension)
+                {
+                    posição.Y = 0;
+                }
+                if (posição.Y < 0)
+                {
+                    posição.Y = 29 * dimension;
+                }
+            }
+
+            
+            //Se não for comida a proxima posição
+            if (!gotFood())
+            {
+                Rectangle temp = rects.First();
+                rects.Remove(temp);
+                Canvas.SetTop(temp, posição.Y);
+                Canvas.SetLeft(temp, posição.X);
+                rects.Add(temp);
+            }
+            else
+            {
+                rects.Add(refRect(posição.X / dimension, posição.Y / dimension));
+                canvas.Children.Add(rects.Last());
+            }
+        }
+
+        //Metodo que testa se a proxmia posição for comida
+        public bool gotFood()
+        {
+            if (posição.X == Canvas.GetLeft(food) && posição.Y == Canvas.GetTop(food))
+            {
+                Task.Run(() => notificationSound.Play());
+
+                Random rnd = new Random();
+                Console.WriteLine("Pegou a comida");
+
+                PointCollection d = new PointCollection();
+                for (int i = 0; i < rects.Count; i++)
+                {
+                    d.Add(new Point(Canvas.GetLeft(rects[i]), Canvas.GetTop(rects[i])));
+                }
+                int rnd1, rnd2;
+                //Muda a posição da comida caso não seja dentro da snake
+                do
+                {
+                    rnd1 = rnd.Next(30);
+                    rnd2 = rnd.Next(30);
+                    Canvas.SetLeft(food, rnd1 * dimension);
+                    Canvas.SetTop(food, rnd2 * dimension);
+                }
+                while (d.Contains(new Point(rnd1 * dimension, rnd2 * dimension)));
+
+                return true;               
+            }
+            return false;
+        }
+
+        [System.Runtime.InteropServices.DllImport("winmm.dll")]
+        public static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+        //MediaPlayer mp = new MediaPlayer();
+        SoundPlayer notificationSound = new SoundPlayer("Resources\\buh.wav");
+    }
+}
---
+++ Snake/MainWindow.xaml.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int theme = 1;
+        int velocity = 50;
+        MediaPlayer player = new MediaPlayer();
+        Jogo game;
+        Timer t;
+        
+        SolidColorBrush borderColor = new SolidColorBrush();
+
+        //Construtor
+        public MainWindow()
+        {
+            InitializeComponent();
+            run();
+
+            //Pega a musica do arquivo e carrega no player
+            Uri uri = new Uri("Resources\\mus_zz_megalovania.ogg", UriKind.Relative);
+            player.Open(uri);
+
+            player.Play();
+            player.MediaEnded += Player_MediaEnded;
+
+            canvas.LayoutTransform = new ScaleTransform();    
+        }
+
+        //Quando acaba a musica, recomeça
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            player.Play();
+        }
+       
+        //Muda o tema atual do jogo
+        public void setTheme()
+        {
+            if (theme == 1)
+            {
+                //Black Theme
+                canvas.Background = new SolidColorBrush(Colors.Black);
+                Background = new SolidColorBrush(Colors.Black);
+                borderColor = new SolidColorBrush(Colors.White);
+                border.BorderBrush = borderColor;
+                game.rectColor = new SolidColorBrush(Colors.White);
+                game.rectStrokeColor = new SolidColorBrush(Colors.LightGray);
+                game.foodStrokeColor = new SolidColorBrush(Colors.DarkGray);
+                game.food.Stroke = game.foodStrokeColor;
+                BorderBrush = new SolidColorBrush(Colors.White);
+                paredeCheck();
+                theme = 0;
+            }
+            else
+            {
+                //White theme
+                canvas.Background = new SolidColorBrush(Colors.White);
+                Background = new SolidColorBrush(Colors.White);
+                borderColor = new SolidColorBrush(Colors.Black);
+                border.BorderBrush = borderColor;
+                game.rectColor = new SolidColorBrush(Colors.Black);
+                game.rectStrokeColor = new SolidColorBrush(Colors.LightGray);
+                game.foodStrokeColor = new SolidColorBrush(Colors.DarkGray);
+                game.food.Stroke = game.foodStrokeColor;
+                BorderBrush = new SolidColorBrush(Colors.Black);
+                paredeCheck();
+                theme = 1;
+            }
+        }
+
+        //Quando o timer é disposed
+        private void T_Disposed(object sender, EventArgs e)
+        {
+            canvas.Children.Clear();
+            run();
+        }
+       
+        //Tick do jogo 
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() => {
+                game.tick2();              
+            });                      
+        }
+
+        //Inicia o jogo
+        public void run()
+        {      
+            t = new System.Timers.Timer(velocity);
+            game = new Jogo(canvas, t, parede_CBox);
+
+            setTheme();
+            game.createBaseRects();
+
+            t.Elapsed += T_Elapsed;
+            t.Disposed += T_Disposed;
+            t.Start();                   
+        }
+
+        //Pega a input e processa
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if ((e.Key == Key.W || e.Key == Key.Up))
+            {
+                if (game.subDir1 != Jogo.Direcao.Nenhuma && game.subDir1 != Jogo.Direcao.Down && game.subDir1 != Jogo.Direcao.Up)
+                    game.subDir2 = Jogo.Direcao.Up;
+                else if (game.direction != Jogo.Direcao.Down && game.subDir1 != Jogo.Direcao.Down && game.direction != Jogo.Direcao.Up)
+                     game.subDir1 = Jogo.Direcao.Up;
+            }
+            if ((e.Key == Key.A || e.Key == Key.Left))
+            {
+                if (game.subDir1 != Jogo.Direcao.Nenhuma && game.subDir1 != Jogo.Direcao.Right && game.subDir1 != Jogo.Direcao.Left)
+                    game.subDir2 = Jogo.Direcao.Left;
+                else if (game.direction != Jogo.Direcao.Right && game.subDir1 != Jogo.Direcao.Right && game.direction != Jogo.Direcao.Left)
+                    game.subDir1 = Jogo.Direcao.Left;
+            }
+            if ((e.Key == Key.D || e.Key == Key.Right))
+            {
+                if (game.subDir1 != Jogo.Direcao.Nenhuma && game.subDir1 != Jogo.Direcao.Left && game.subDir1 != Jogo.Direcao.Right)
+                    game.subDir2 = Jogo.Direcao.Right;
+                else if (game.direction != Jogo.Direcao.Left && game.subDir1 != Jogo.Direcao.Left && game.direction != Jogo.Direcao.Right)
+                    game.subDir1 = Jogo.Direcao.Right;
+            }
+            if ((e.Key == Key.S || e.Key == Key.Down))
+            {
+                if (game.subDir1 != Jogo.Direcao.Nenhuma && game.subDir1 != Jogo.Direcao.Up && game.subDir1 != Jogo.Direcao.Down)
+                    game.subDir2 = Jogo.Direcao.Down;
+                else if (game.direction != Jogo.Direcao.Up && game.subDir1 != Jogo.Direcao.Up && game.direction != Jogo.Direcao.Down)
+                    game.subDir1 = Jogo.Direcao.Down;
+            }
+
+            if (e.Key == Key.Escape)            
+                Close();
+            
+        }
+
+        //Aumenta e diminui a velocidade
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Button snd = sender as Button;
+
+            if (snd.Content.ToString() == "-")
+            {
+                if (velocity <= 10)
+                    velocity -= 1;
+                else
+                    velocity -= 10;
+
+                if (velocity <= 0)
+                    velocity = 1;
+
+                velocidade.Content = velocity;
+                t.Interval = velocity;
+            }
+            if (snd.Content.ToString() == "+")
+            {
+                if (velocity + 1 <= 10)
+                    velocity += 1;
+                else
+                    velocity += 10;
+
+                velocidade.Content = velocity;
+                t.Interval = velocity;
+            }
+        }
+
+        //Muda a propriedade da parede
+        private void parede_CBox_Changed(object sender, RoutedEventArgs e)
+        {
+            paredeCheck();
+        }
+        void paredeCheck()
+        {
+            if (parede_CBox.IsChecked.Value)
+            {
+                border.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                border.BorderBrush = borderColor;
+            }
+        }  
+
+        //Muda o tema
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            setTheme();
+        }
+
+        //Para e starta a musica ao mudar a checkbox
+        private void musica_CBox_Changed(object sender, RoutedEventArgs e)
+        {
+            if (musica_CBox.IsChecked.Value)
+                player.Play();          
+            else 
+                player.Pause();   
+            //  player.Stop();        
+        }
+
+        #region Meh
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            // if (sizeInfo.WidthChanged && ActualWidth / aspect != ActualHeight)
+            //      this.Width = sizeInfo.NewSize.Height * aspect;
+            //  else this.Height = sizeInfo.NewSize.Width / aspect;
+
+
+            // ((ScaleTransform)canvas.LayoutTransform).ScaleX = this.Width / 350;
+            // ((ScaleTransform)canvas.LayoutTransform).ScaleY = this.Width / 350;
+        }
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+        const double aspect = 350 / 400.0;
+        //Inside dispatcher method run
+        /* if (game.rects.Count() % 10 == 0)
+                {
+                    bmpCout++;
+                    var image = rsr[bmpCout];
+                    var bitmap = new System.Drawing.Bitmap(image);
+                    var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    bitmap.Dispose();
+                    var brush = new ImageBrush(bitmapSource);
+
+                    canvas.Background = brush;
+                }*/
+
+        /*Bitmap[] rsr = new[] {
+           Properties.Resources.p17,
+           Properties.Resources.p16,
+           Properties.Resources.p15,
+           Properties.Resources.p14,
+           Properties.Resources.p13,
+           Properties.Resources.p12,
+           Properties.Resources.p11,
+                  Properties.Resources.p10,
+                   Properties.Resources.p9,
+                    Properties.Resources.p8,
+                     Properties.Resources.p7,
+                      Properties.Resources.p6,
+                       Properties.Resources.p5,
+                        Properties.Resources.p4,
+                         Properties.Resources.p3,
+                          Properties.Resources.p2,
+                           Properties.Resources.p1,
+                            Properties.Resources.p0,
+       };*/
+
+        //Inside constructor
+        /* var image = Properties.Resources.p17;
+            var bitmap = new System.Drawing.Bitmap(image);
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),   IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions() );
+            bitmap.Dispose();
+            var brush = new ImageBrush(bitmapSource);
+
+            canvas.Background = brush;*/
+        #endregion
+
+    }
+}
